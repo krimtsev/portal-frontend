641844260e7e048d1b0bb50bedb7aba06002df0c
Nikolai Kopytov <nkopytov@itoolabs.com>
2025-07-06 21:42:58 +0300

    add PrimeVueResolver

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import path from "node:path"
 import { defineConfig, loadEnv } from "vite"
 import vue from "@vitejs/plugin-vue"
+import Components from "unplugin-vue-components/vite"
+import { PrimeVueResolver } from "@primevue/auto-import-resolver"
 
 
 export default ({ mode }: { mode: never }) => {
@@ -15,6 +17,11 @@ export default ({ mode }: { mode: never }) => {
 
         plugins: [
             vue(),
+            Components({
+                resolvers: [
+                    PrimeVueResolver()
+                ]
+            })
         ],
 
         resolve: {
