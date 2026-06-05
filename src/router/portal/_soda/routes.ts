104811fbdaba35b5a982951153dc0c2d726e95a9
Nikolai Kopytov <nkopytov@itoolabs.com>
2026-06-05 14:47:57 +0300

    Заглушка под экран соды

--- src/router/portal/_soda/routes.ts
+++ src/router/portal/_soda/routes.ts
@@ -1,7 +1,19 @@
 import type { RouteRecordRaw } from "vue-router"
+import { portalPaths } from "@r/portal/path"
+import { PortalRouteName } from "@r/portal/route-names"
+import { Roles } from "@/definitions/roles"
+
+const roles = [Roles.USER, Roles.ADMIN, Roles.SYSADMIN]
 
 const routes: RouteRecordRaw[] = [
-
+    {
+        path:      portalPaths.Home,
+        name:      PortalRouteName.Home,
+        component: () => import("@v/portal/home/_soda/home-view.vue"),
+        meta:      {
+            roles,
+        },
+    },
 ]
 
 export default routes
