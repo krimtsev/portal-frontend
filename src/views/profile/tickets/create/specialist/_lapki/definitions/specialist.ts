840dcebaa7ca31cf4c379b97f448ca638f921bca
Nikolai Kopytov <nkopytov@itoolabs.com>
2026-09-14 23:54:12 +0300

    Изменили список специалистов лапки

--- src/views/profile/tickets/create/specialist/_lapki/definitions/specialist.ts
+++ src/views/profile/tickets/create/specialist/_lapki/definitions/specialist.ts
@@ -1,9 +1,5 @@
 export enum LapkiQualification {
     Groomer = "Groomer",
-    GroomerPlus = "GroomerPlus",
     TopGroomer = "TopGroomer",
-    TopGroomerPlus = "TopGroomerPlus",
     BrandGroomer = "BrandGroomer",
-    BrandGroomerPlus = "BrandGroomerPlus",
-    GroomerExpert = "GroomerExpert",
 }
--- src/views/profile/tickets/create/specialist/_lapki/utils/specialist-utils.ts
+++ src/views/profile/tickets/create/specialist/_lapki/utils/specialist-utils.ts
@@ -1,11 +1,7 @@
 import { LapkiQualification } from "@v/profile/tickets/create/specialist/_lapki/definitions/specialist"
 
 export const GroomerQualificationTranslate: Record<LapkiQualification, string> = {
-    [LapkiQualification.Groomer]:          "mc.ticket.specialistQualification.groomer",
-    [LapkiQualification.GroomerPlus]:      "mc.ticket.specialistQualification.groomerPlus",
-    [LapkiQualification.TopGroomer]:       "mc.ticket.specialistQualification.topGroomer",
-    [LapkiQualification.TopGroomerPlus]:   "mc.ticket.specialistQualification.topGroomerPlus",
-    [LapkiQualification.BrandGroomer]:     "mc.ticket.specialistQualification.brandGroomer",
-    [LapkiQualification.BrandGroomerPlus]: "mc.ticket.specialistQualification.brandGroomerPlus",
-    [LapkiQualification.GroomerExpert]:    "mc.ticket.specialistQualification.groomerExpert",
+    [LapkiQualification.Groomer]:      "mc.ticket.specialistQualification.groomer",
+    [LapkiQualification.TopGroomer]:   "mc.ticket.specialistQualification.topGroomer",
+    [LapkiQualification.BrandGroomer]: "mc.ticket.specialistQualification.brandGroomer",
 }
