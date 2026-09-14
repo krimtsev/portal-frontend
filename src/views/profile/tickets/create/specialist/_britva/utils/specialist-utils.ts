8214d940eb36a90caf0c3cc43c7315ffe78b9346
Nikolai Kopytov <nkopytov@itoolabs.com>
2026-09-14 23:47:25 +0300

    Переводы

--- src/views/profile/tickets/create/specialist/_britva/utils/specialist-utils.ts
+++ src/views/profile/tickets/create/specialist/_britva/utils/specialist-utils.ts
@@ -1,11 +1,11 @@
 import { BarberQualification } from "@v/profile/tickets/create/specialist/_britva/definitions/specialist"
 
 export const BarberQualificationTranslate: Record<BarberQualification, string> = {
-    [BarberQualification.BarberPlus]:      "mc.ticket.barberQualification.barberPlus",
-    [BarberQualification.TobBarber]:       "mc.ticket.barberQualification.tobBarber",
-    [BarberQualification.TobBarberPlus]:   "mc.ticket.barberQualification.tobBarberPlus",
-    [BarberQualification.BrandBarber]:     "mc.ticket.barberQualification.brandBarber",
-    [BarberQualification.BrandBarberPlus]: "mc.ticket.barberQualification.brandBarberPlus",
-    [BarberQualification.BrandBarberPro]:  "mc.ticket.barberQualification.brandBarberPro",
-    [BarberQualification.Expert]:          "mc.ticket.barberQualification.expert",
+    [BarberQualification.BarberPlus]:      "mc.ticket.specialistQualification.barberPlus",
+    [BarberQualification.TobBarber]:       "mc.ticket.specialistQualification.tobBarber",
+    [BarberQualification.TobBarberPlus]:   "mc.ticket.specialistQualification.tobBarberPlus",
+    [BarberQualification.BrandBarber]:     "mc.ticket.specialistQualification.brandBarber",
+    [BarberQualification.BrandBarberPlus]: "mc.ticket.specialistQualification.brandBarberPlus",
+    [BarberQualification.BrandBarberPro]:  "mc.ticket.specialistQualification.brandBarberPro",
+    [BarberQualification.Expert]:          "mc.ticket.specialistQualification.expert",
 }
